b6a279ea6a46fe35febb37219f655c5eb3328668
nisanthkp <48823677+nisanthkp@users.noreply.github.com>
2020-03-02 21:42:22 +0530

    Update testingscript.sh

--- testingscript.sh
+++ testingscript.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-VMHostName=$(hostname)
-#echo "127.0.1.1 $CommonName" >> /etc/hosts
-sed -i "/^127.0.0.1 localhost/a 127.0.1.1 $VMHostName" /etc/hosts
+
+touch /home/cliadmin/testnew.txt
